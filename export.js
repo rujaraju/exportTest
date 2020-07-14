1deb1fe46c451e39c5b8fe336b501c5088c4baee
Jan Rundt <jan.rundt@gmail.com>
2020-07-14 23:26:13 +0300

    comments

--- export.js
+++ export.js
@@ -12,7 +12,7 @@ function timeout(ms) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(html)
-    await timeout(1000)
+    await timeout(1000)//this is a fix because the apexchart is animated so needs to wait until it's completely rendered
     await page.pdf({path: 'apex.pdf'})
     await page.screenshot({path: 'apex.jpg', type: 'jpeg', quality: 100});//comes out lo resolution so needs a fix
     await browser.close();
