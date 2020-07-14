532c976dc4a818ba195b0ec26a17a49110dd5c9d
Jan Rundt <jan.rundt@gmail.com>
2020-07-14 23:16:36 +0300

    first

---
+++ export.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+let examplePath = path.join(__dirname, 'apexExample.html');
+const fs = require('fs').promises;
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+(async () => {
+    const buffer = await fs.readFile(examplePath);
+    const html = buffer.toString();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html)
+    await timeout(1000)
+    await page.pdf({path: 'apex.pdf'})
+    await page.screenshot({path: 'apex.jpg', type: 'jpeg', quality: 100});//comes out lo resolution so needs a fix
+    await browser.close();
+})();
